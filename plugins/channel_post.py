de62d06dffdacdce48c65e27688cb27298042d04
simanto008 <149402418+simanto008@users.noreply.github.com>
2025-04-03 11:39:49 +0600

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -26,7 +26,7 @@ async def channel_post(client: Client, message: Message):
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
 
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 File URL", url=f'{link}')]])
 
     await reply_text.edit(f"<b>Here is your link</b>\n\n{link}", reply_markup=reply_markup, disable_web_page_preview = True)
 
